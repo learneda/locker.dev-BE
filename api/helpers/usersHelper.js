4200b582d3e1440685dbc1fb8ca5c1a2735193ce
luiscmartinez <mrluismartinezzz@gmail.com>
2019-03-21 15:46:16 -0700

    created user routes

--- api/helpers/usersHelper.js
+++ api/helpers/usersHelper.js
@@ -7,5 +7,12 @@ module.exports = {
     let query = db('users');
     if (id) query.where('id', Number(id)).first();
     return query;
+  },
+  createUser: function (user) {
+    return db('users').insert(user)
+    .then(ids => {
+      console.log('created user return ==>',ids)
+      return { id: ids[0] }
+    });
   }
 };

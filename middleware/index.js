898bae4da41d090f7a6dee6e1f68948d9229998b
Riley-Brown <riley@riley.gg>
2019-03-21 12:11:46 -0400

    Added cors to middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const helmet = require('helmet')
-const passport = require('passport')
-const logger = require('morgan')
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const logger = require('morgan');
+const cors = require('cors');
 
-module.exports = (server) => {
-  server.use(express.json())
-  server.use(helmet())
-  server.use(logger('dev'))
-  server.use(passport.initialize())
-  server.use(passport.session())
-}
+module.exports = server => {
+  server.use(express.json());
+  server.use(cors());
+  server.use(helmet());
+  server.use(logger('dev'));
+  server.use(passport.initialize());
+  server.use(passport.session());
+};

bd63f6e0ba3fadd21c17058dc0375d5a0cbbff7b
Riley-Brown <riley@riley.gg>
2019-03-21 21:45:28 -0400

    Added route to parse URL and return metadata

--- api/Routes/posts.js
+++ api/Routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-
+const DB = require('../../dbConfig');
+const urlMetadata = require('url-metadata');
 // ==============================================
 // this JS file includes helpers that access our
 // database accordingly (for example, getUsers
@@ -10,6 +11,17 @@ const postsDb = require('../helpers/postsHelper');
 router.get('/posts', async (req, res) => {
   const posts = await postsDb.get();
   res.status(200).send(posts);
+});
+
+router.get('/post', (req, res) => {
+  urlMetadata(`${req.query.url}`).then(
+    function(metadata) {
+      res.json(metadata);
+    },
+    function(error) {
+      console.log(error);
+    }
+  );
 });
 
 module.exports = router;

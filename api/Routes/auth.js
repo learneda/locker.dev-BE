311a4106d54f41dbb05d08d8c25d99c65ca7d163
Riley-Brown <riley@riley.gg>
2019-03-25 17:22:51 -0400

    added route for google oauth, and callback route to return oauth data

--- api/Routes/auth.js
+++ api/Routes/auth.js
@@ -1,15 +1,28 @@
-const router = require('express').Router()
-const passport = require('passport')
+const router = require('express').Router();
+const passport = require('passport');
 // const db = require('../../dbConfig')
 
-router.get('/github', passport.authenticate('github'))
+router.get('/github', passport.authenticate('github'));
 
 router.get('/github/cb', passport.authenticate('github'), (req, res, next) => {
   // Successful authentication, redirect home.
-  console.log('IS USER Authenti ??? ??', req.isAuthenticated())
+  console.log('IS USER Authenti ??? ??', req.isAuthenticated());
   if (process.env.NODE_ENV === 'production') {
     res.redirect('https://learnedadev.netlify.com/feed');
   } else res.redirect('http://localhost:3000/feed');
-})
+});
 
-module.exports = router
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  (req, res) => {
+    res.redirect('success');
+  }
+);
+
+module.exports = router;

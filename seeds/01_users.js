890bb7af6abb031b8ce322f4a89b89adfa1f4a40
luiscmartinez <mrluismartinezzz@gmail.com>
2019-03-25 08:51:34 -0700

    populated some user table seed data

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -5,8 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('users').insert([
-        { email: 'homer@gmail.com', display_name: 'Homer Simpson' },
-        { email: 'jonsnow@goft.com', display_name: 'Jon Snow' }
+        // user signs up with bycrpt func
+        {first_name: 'Homer', last_name:'Simpson', email: 'homer@gmail.com', password: 'donut', display_name: 'Homer Simpson', profile_picture: 'https://www.bing.com/th?id=OIP.zJDfhDqqdSzLH7DVLColvQHaE8&pid=Api&dpr=2.5&rs=1&p=0'},
+        // user signs up with github
+        { email: 'jonsnow@goft.com', display_name: 'Jon Snow', profile_picture:'https://www.carid.com/images/snow-joe/patio-and-garden/sj624e-6.jpg'
+       }, 
       ]);
     });
 };

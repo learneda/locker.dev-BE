26da608186f080dbd48e5442d2e4a81238e8503d
jsmnglng <jsmn.glng@gmail.com>
2019-03-28 13:22:44 -0700

    Refactored /courses and /articles

--- api/Routes/browse.js
+++ api/Routes/browse.js
@@ -1,7 +1,6 @@
 const request = require('request');
 const router = require('express').Router();
 const Feed = require('rss-to-json');
-const { extract } = require('article-parser');
 const routeCache = require('route-cache');
 const urlMetadata = require('url-metadata');
 
@@ -22,6 +21,7 @@ router.get('/courses', routeCache.cacheSeconds(120), (req, res) => {
           let url = course.url;
           return urlMetadata(`https://www.udemy.com${url}`)
             .then(metadata => ({
+              course_id: course.id,
               title: metadata.title,
               description: metadata.description,
               thumbnail: metadata.image,
@@ -46,13 +46,13 @@ router.get('/articles', routeCache.cacheSeconds(60), (req, res) => {
     const tempo_articles = rss.items.map(item => {
       // for each article, get its url and parse it
       let url = item.url;
-      return extract(url)
+      return urlMetadata(url)
         .then(article => ({
+          created: item.created,
           title: article.title,
           description: article.description,
           thumbnail: article.image,
-          created: article.publishedTime,
-          url: url
+          url: article.url
         }))
         .catch(err => {
           console.log(err);

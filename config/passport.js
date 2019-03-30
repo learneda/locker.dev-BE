bc1183a4a45b1195818548306b0a4b7b619419ec
luiscmartinez <mrluismartinezzz@gmail.com>
2019-03-29 18:13:18 -0700

    added try & catch block statements for Github & Google Strategies

--- config/passport.js
+++ config/passport.js
@@ -1,60 +1,59 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const GitHubStrategy = require('passport-github').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const LocalStrategy = require('passport-local').Strategy
 
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-},
-  async function(email, password, done) { 
-    console.log(email, password)
-    const existingUser = await db('users')
-      .where('email', email)
-      .first();
-    if (existingUser) {
-      console.log(existingUser)
-      const passwordCheck = bcrypt.compareSync(password, existingUser.password)
-      console.log(passwordCheck === true)
-      if (passwordCheck === true) {
-        console.log('here true')
-        done(null, existingUser);
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async function (email, password, done) {
+      console.log(email, password)
+      const existingUser = await db('users').where('email', email).first()
+      if (existingUser) {
+        console.log(existingUser)
+        const passwordCheck = bcrypt.compareSync(
+          password,
+          existingUser.password
+        )
+        console.log(passwordCheck === true)
+        if (passwordCheck === true) {
+          console.log('here true')
+          done(null, existingUser)
+        } else {
+          done(new Error('credentials wrong'))
+        }
       } else {
-        done(new Error('credentials wrong'))
+        password = bcrypt.hashSync(password, 10)
+        await db('users').insert({
+          email: email,
+          password: password,
+          display_name: 'a dynamic name'
+        })
+        const user = await db('users').where({ email: email }).first()
+        done(null, user)
       }
-    } else {
-      password = bcrypt.hashSync(password, 10);
-      await db('users').insert({
-        email: email,
-        password: password,
-        display_name: 'a dynamic name',
-      });
-      const user = await db('users')
-        .where({email: email })
-        .first();
-      done(null, user);
     }
-  }));
+  )
+)
 
+const db = require('../dbConfig')
 
-const db = require('../dbConfig');
-
-passport.serializeUser((user, done) => done(null, user.id));
+passport.serializeUser((user, done) => done(null, user.id))
 
 passport.deserializeUser((id, done) => {
-  db('users')
-    .where({ id: id })
-    .first()
-    .then(user => {
-      if (!user) {
-        done(new Error('User not found ' + id));
-      }
-      done(null, user);
-    });
-});
+  db('users').where({ id: id }).first().then((user) => {
+    if (!user) {
+      done(new Error('User not found ' + id))
+    }
+    done(null, user)
+  })
+})
 
 passport.use(
   new GitHubStrategy(
@@ -65,25 +64,29 @@ passport.use(
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await db('users')
-        .where('github_id', profile.id)
-        .first();
-      if (existingUser) {
-        done(null, existingUser);
-      } else {
-        await db('users').insert({
-          github_id: profile.id,
-          display_name: profile.username,
-          profile_picture: profile.photos[0].value
-        });
-        const user = await db('users')
-          .where({ github_id: profile.id })
-          .first();
-        done(null, user);
+      try {
+        const existingUser = await db('users')
+          .where('github_id', profile.id)
+          .first()
+        if (existingUser) {
+          return done(null, existingUser)
+        } else {
+          await db('users').insert({
+            github_id: profile.id,
+            display_name: profile.username,
+            profile_picture: profile.photos[0].value
+          })
+          const user = await db('users')
+            .where({ github_id: profile.id })
+            .first()
+          return done(null, user)
+        }
+      } catch (err) {
+        return done(err)
       }
     }
   )
-);
+)
 
 /* ===== PASSPORT GOOGLE STRATEGY ===== */
 passport.use(
@@ -95,24 +98,28 @@ passport.use(
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-      const google_id = profile.id;
-      const existingUser = await db('users')
-        .where('google_id', google_id)
-        .first();
-      if (existingUser) {
-        done(null, existingUser);
-      } else {
-        await db('users').insert({
-          google_id: profile.id,
-          display_name: profile.displayName,
-          email: profile.emails[0].value,
-          profile_picture: profile.photos[0].value
-        });
-        const user = await db('users')
-          .where({ google_id: profile.id })
-          .first();
-        done(null, user);
+      try {
+        const google_id = profile.id
+        const existingUser = await db('users')
+          .where('google_id', google_id)
+          .first()
+        if (existingUser) {
+          return done(null, existingUser)
+        } else {
+          await db('users').insert({
+            google_id: profile.id,
+            display_name: profile.displayName,
+            email: profile.emails[0].value,
+            profile_picture: profile.photos[0].value
+          })
+          const user = await db('users')
+            .where({ google_id: profile.id })
+            .first()
+          return done(null, user)
+        }
+      } catch (err) {
+        return done(err)
       }
     }
   )
-);
+)

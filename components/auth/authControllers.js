f57dad7c9aa1d340d1920fd957c9af7aff014fcf
Riley-Brown <riley@riley.gg>
2019-04-01 16:28:43 -0400

    Changed req.logout to req.session = null

--- components/auth/authControllers.js
+++ components/auth/authControllers.js
@@ -2,21 +2,22 @@ const db = require('../../dbConfig');
 require('dotenv').config();
 
 module.exports = {
-  gitHubHandler (req, res, next) {
+  gitHubHandler(req, res, next) {
     if (process.env.NODE_ENV === 'production') {
       res.redirect('https://learnedadev.netlify.com/profile');
     } else res.redirect('http://localhost:3000/profile');
   },
 
-  googleHandler (req, res, next) {
+  googleHandler(req, res, next) {
     if (process.env.NODE_ENV === 'production') {
       res.redirect('https://learnedadev.netlify.com/profile');
     } else res.redirect('http://localhost:3000/profile');
   },
-  logoutHandler (req, res, next) {
-    req.logout();
+  logoutHandler(req, res, next) {
+    req.session = null;
+
     if (process.env.NODE_ENV === 'production') {
       res.redirect('https://learnedadev.netlify.com');
     } else res.redirect('http://localhost:3000');
   }
-}
+};

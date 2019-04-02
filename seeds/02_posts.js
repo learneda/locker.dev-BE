b7e82f4940ea744f7d285527a2b41dcf35eb8825
samsisle <samko9522@gmail.com>
2019-04-01 23:03:27 -0700

    💟 Update seeds to reflect updated migration tables

--- seeds/02_posts.js
+++ seeds/02_posts.js
@@ -5,10 +5,36 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('posts').insert([
-        { post_url: "https://riley.gg", user_id:1, categories: ['JS', 'Jquery', 'CSS'], recommended: true, completed: true, rating: 3},
-        { post_url: "https://www.youtube.com/watch?v=HSwjGP19rTg", user_id:1, categories: ['youtube', 'freedomjs'], recommended: true, rating:5  },
-        { post_url: "https://www.youtube.com/watch?v=qtURixlmp6M", user_id:2, categories: ['C'], recommended: false, completed: false, },
-        { post_url: "https://www.youtube.com/watch?v=93p3LxR9xfM", user_id:2, categories:['python'], recommended: false, completed: true,rating:2 }
+        {
+          post_url: 'https://riley.gg',
+          user_id: 1,
+          categories: ['JS', 'Jquery', 'CSS'],
+          liked: true,
+          completed: true,
+          rating: 3
+        },
+        {
+          post_url: 'https://www.youtube.com/watch?v=HSwjGP19rTg',
+          user_id: 1,
+          categories: ['youtube', 'freedomjs'],
+          liked: true,
+          rating: 5
+        },
+        {
+          post_url: 'https://www.youtube.com/watch?v=qtURixlmp6M',
+          user_id: 2,
+          categories: ['C'],
+          liked: false,
+          completed: false
+        },
+        {
+          post_url: 'https://www.youtube.com/watch?v=93p3LxR9xfM',
+          user_id: 2,
+          categories: ['python'],
+          liked: false,
+          completed: true,
+          rating: 2
+        }
       ]);
     });
 };

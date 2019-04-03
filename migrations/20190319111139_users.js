4fbea678f9ee15046ac895b8e321760aab5b0410
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-03 16:20:02 -0700

    refactored users table

--- migrations/20190319111139_users.js
+++ migrations/20190319111139_users.js
@@ -1,17 +1,19 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', tbl => {
     tbl.increments('id');
-    tbl.string('first_name').nullable();
-    tbl.string('last_name').nullable();
     tbl
       .string('email', 254)
-      .nullable()
       .unique();
-    tbl.string('password').nullable();
-    tbl.string('display_name').notNullable();
+    tbl.string('username').nullable();
     tbl.string('profile_picture').nullable();
     tbl.string('github_id').nullable();
     tbl.string('google_id').nullable();
+    tbl.string('bio');
+    tbl.string('location');
+    tbl.string('website_url');
+    tbl.string('facebook_url');
+    tbl.string('github_url');
+    tbl.string('twitter_url');
     tbl.timestamp('created_at').defaultTo(knex.fn.now());
     tbl.timestamp('updated_at').defaultTo(knex.fn.now());
   });

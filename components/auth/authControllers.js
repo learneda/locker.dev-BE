a03e3f7ee105103d48881fb41f30a21822f90ff1
jsmnglng <jsmn.glng@gmail.com>
2019-04-03 21:17:29 -0700

    Implemented editing profile info

--- components/auth/authControllers.js
+++ components/auth/authControllers.js
@@ -19,5 +19,19 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       res.redirect('https://learnedadev.netlify.com');
     } else res.redirect('http://localhost:3000');
+  },
+  async editProfile(req, res, next) {
+    const id = req.params.id;
+    const { bio, location, website_url } = req.body;
+
+    try {
+      await db('users')
+        .where({ id })
+        .update({ bio, location, website_url });
+      res.status(200).json({ success: 'Profile updated!' });
+    } catch (err) {
+      console.log('edit profile err', err);
+      res.status(500).json({ err });
+    }
   }
 };

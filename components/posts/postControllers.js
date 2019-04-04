f8752963fc36b7eff13247c3d2f6395a3fddd2e5
sumi419 <sasgar04@gmail.com>
2019-04-03 17:37:12 -0700

    👩‍💻Update all post routes with req.user conditionals

--- components/posts/postControllers.js
+++ components/posts/postControllers.js
@@ -3,40 +3,52 @@ const urlMetadata = require('url-metadata');
 
 module.exports = {
   async getAllUserPosts(req, res, next) {
-    try {
-      const posts = await db('posts')
-        .where({ user_id: req.user.id })
-        .orderBy('id', 'asc');
-      return res.status(200).json(posts);
-    } catch (err) {
-      console.log(err);
+    if (req.user) {
+      try {
+        const posts = await db('posts')
+          .where({ user_id: req.user.id })
+          .orderBy('id', 'asc');
+        return res.status(200).json(posts);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      res.status(403).json({ error: 'Not authorized' });
     }
   },
   async getAllUserPostsLiked(req, res, next) {
-    try {
-      const posts = await db('posts')
-        .where({ user_id: req.user.id, liked: true })
-        .orderBy('id', 'asc');
-      return res.status(200).json(posts);
-    } catch (err) {
-      console.log(err);
+    if (req.user) {
+      try {
+        const posts = await db('posts')
+          .where({ user_id: req.user.id, liked: true })
+          .orderBy('id', 'asc');
+        return res.status(200).json(posts);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      res.status(403).json({ error: 'Not authorized' });
     }
   },
 
   async getAllSharedPost(req, res, next) {
-    const user_id = req.user.id;
-    try {
-      const sharedPostPromise = await db('posts').where({
-        user_id,
-        recommended: true
-      });
-      if (sharedPostPromise) {
-        console.log(sharedPostPromise);
-        res.status(200).json({ postsArr: sharedPostPromise });
+    if (req.user) {
+      const user_id = req.user.id;
+      try {
+        const sharedPostPromise = await db('posts').where({
+          user_id,
+          recommended: true
+        });
+        if (sharedPostPromise) {
+          console.log(sharedPostPromise);
+          res.status(200).json({ postsArr: sharedPostPromise });
+        }
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
       }
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: err });
+    } else {
+      res.status(403).json({ error: 'Not authorized' });
     }
   },
   async getPost(req, res, next) {
@@ -55,39 +67,44 @@ module.exports = {
     }
   },
   async createNewPost(req, res, next) {
-    console.log('this is the req user', req.user, req.isAuthenticated());
-    if (req.body.post_url && req.body.id) {
-      try {
-        const newUrl =
-          req.body.post_url.indexOf('http') > -1
-            ? req.body.post_url
-            : `http://${req.body.post_url}`;
-        const metadata = await urlMetadata(newUrl);
+    if (req.user) {
+      if (req.body.post_url && req.body.id) {
         try {
-          const newInsert = await db('posts').insert({
-            post_url: req.body.post_url,
-            user_id: req.body.id,
-            title: metadata.title,
-            description: metadata.description,
-            thumbnail_url: metadata.image
-          });
-          if (newInsert) {
-            res.status(201).json({ message: 'Post was successfully added :)' });
-          } else {
-            res.status(300).json({ err: 'couldnt add new entry' });
+          const newUrl =
+            req.body.post_url.indexOf('http') > -1
+              ? req.body.post_url
+              : `http://${req.body.post_url}`;
+          const metadata = await urlMetadata(newUrl);
+          try {
+            const newInsert = await db('posts').insert({
+              post_url: req.body.post_url,
+              user_id: req.body.id,
+              title: metadata.title,
+              description: metadata.description,
+              thumbnail_url: metadata.image
+            });
+            if (newInsert) {
+              res
+                .status(201)
+                .json({ message: 'Post was successfully added :)' });
+            } else {
+              res.status(300).json({ err: 'couldnt add new entry' });
+            }
+          } catch (err) {
+            console.log('META ERROR', err);
+            res.status(500).json(err);
           }
         } catch (err) {
           console.log('META ERROR', err);
-          res.status(500).json(err);
+          res.status(300).json({ err: 'couldnt add new entry' });
         }
-      } catch (err) {
-        console.log('META ERROR', err);
-        res.status(300).json({ err: 'couldnt add new entry' });
+      } else {
+        res
+          .status(400)
+          .json({ message: 'Please provide a post url and a user id :)' });
       }
     } else {
-      res
-        .status(400)
-        .json({ message: 'Please provide a post url and a user id :)' });
+      res.status(403).json({ error: 'Not authorized' });
     }
   },
   async deletePost(req, res, next) {

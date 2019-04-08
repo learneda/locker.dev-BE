89e24782d81b00e4b7b05948d26a543ed9adfbe2
Riley-Brown <riley@riley.gg>
2019-04-08 15:34:40 -0400

    created getUserTotalFollowers method

--- components/users/userControllers.js
+++ components/users/userControllers.js
@@ -1,32 +1,43 @@
 const db = require('../../dbConfig');
 module.exports = {
   async postUserDetails(req, res, next) {
-    const user = req.body.id
-    const {bio, location, website_url} = req.body
+    const user = req.body.id;
+    const { bio, location, website_url } = req.body;
     if (user) {
       try {
-        const postPromise = await db('users').where({id: user}).update({bio,location,website_url});
+        const postPromise = await db('users')
+          .where({ id: user })
+          .update({ bio, location, website_url });
         if (postPromise) {
-          res.status(200).json({msg: 'success'});
+          res.status(200).json({ msg: 'success' });
         } else {
-          res.status(400).json({err: 'something went wrong'});
+          res.status(400).json({ err: 'something went wrong' });
         }
       } catch (err) {
         console.log(err);
         res.status(500).json(err);
       }
     } else {
-      res.status(400).json({err: 'not allowed'})
+      res.status(400).json({ err: 'not allowed' });
     }
   },
   async getUserDetailsByUserName(req, res, next) {
-    const username = req.params.username
+    const username = req.params.username;
     try {
-      const selectPromise = await db('users').where({username: username}).select('username', 'display_name', 'profile_picture', 'bio', 'location', 'website_url');
+      const selectPromise = await db('users')
+        .where({ username: username })
+        .select(
+          'username',
+          'display_name',
+          'profile_picture',
+          'bio',
+          'location',
+          'website_url'
+        );
       if (selectPromise) {
         res.status(200).json(selectPromise);
       } else {
-        res.status(404).json({msg: 'user not found..'});
+        res.status(404).json({ msg: 'user not found..' });
       }
     } catch (err) {
       console.log(err);
@@ -34,14 +45,17 @@ module.exports = {
     }
   },
   async subscribetoUser(req, res, next) {
-    const user_id = req.user === undefined ? req.body.user_id : req.user.id
+    const user_id = req.body.user_id;
     const friend_id = req.body.friend_id;
     try {
-      const insertPromise = await db('friendships').insert({user_id, friend_id})
+      const insertPromise = await db('friendships').insert({
+        user_id,
+        friend_id
+      });
       if (insertPromise) {
-        res.status(200).json({msg: 'success'});
+        res.status(200).json({ msg: 'success' });
       } else {
-        res.status(404).json({msg: 'something went wrong ..'});
+        res.status(404).json({ msg: 'something went wrong ..' });
       }
     } catch (err) {
       console.log(err);
@@ -49,14 +63,16 @@ module.exports = {
     }
   },
   async unsubscribetoUser(req, res, next) {
-    const user_id = req.user === undefined ? req.body.user_id : req.user.id    
+    const user_id = req.user === undefined ? req.body.user_id : req.user.id;
     const friend_id = req.body.friend_id;
     try {
-      const deletePromise = await db('friendships').where({user_id, friend_id}).del()
+      const deletePromise = await db('friendships')
+        .where({ user_id, friend_id })
+        .del();
       if (deletePromise) {
-        res.status(200).json({msg: 'success'});
+        res.status(200).json({ msg: 'success' });
       } else {
-        res.status(404).json({msg: 'something went wrong ..'});
+        res.status(404).json({ msg: 'something went wrong ..' });
       }
     } catch (err) {
       console.log(err);
@@ -64,20 +80,61 @@ module.exports = {
     }
   },
   async getUserNewsFeed(req, res, next) {
-    const user_id = req.user === undefined ? req.body.user_id : req.user.id
+    const user_id = req.user === undefined ? req.body.user_id : req.user.id;
     try {
-      const newsFeedPromise = await db('friendships').join('posts', function () {
-              this.on('friendships.friend_id', '=','posts.user_id').orOn('posts.user_id', '=', user_id)
-            }).where('friendships.user_id', user_id).orWhere('posts.user_id', '=', user_id).select('posts.user_id', 'post_url', 'title','description', 'thumbnail_url', 'posts.created_at', 'posts.updated_at').distinct();
+      const newsFeedPromise = await db('friendships')
+        .join('posts', function() {
+          this.on('friendships.friend_id', '=', 'posts.user_id').orOn(
+            'posts.user_id',
+            '=',
+            user_id
+          );
+        })
+        .where('friendships.user_id', user_id)
+        .orWhere('posts.user_id', '=', user_id)
+        .select(
+          'posts.user_id',
+          'post_url',
+          'title',
+          'description',
+          'thumbnail_url',
+          'posts.created_at',
+          'posts.updated_at'
+        )
+        .distinct();
       if (newsFeedPromise) {
         console.log(newsFeedPromise);
         res.status(200).json(newsFeedPromise);
       } else {
-        res.status(404).json({msg: 'looks like you need some friends'});
+        res.status(404).json({ msg: 'looks like you need some friends' });
       }
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
+  },
+
+  async getUserTotalFollowers(req, res, next) {
+    const user_id = req.body.user_id;
+    // const friend_id = req.body.user_id;
+    try {
+      const totalUserFollowers = await db('friendships').where(
+        'friendships.user_id',
+        '1'
+      );
+
+      if (totalUserFollowers) {
+        console.log('yoyoyoooo');
+        res.send(200).json({ yo: 'working' });
+      } else {
+        res.send(201).json({ error: 'dis shit broke' });
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
+};
+
+// A USER'S TOTAL FOLLOWERS COUNT = SELECT COUNT(DISTINCT user_id) FROM public.friendships WHERE friend_id = 503
+
+// A USER'S TOTAL FOLLOWING COUNT = SELECT COUNT(DISTINCT friend_id) FROM public.friendships WHERE user_id = 503

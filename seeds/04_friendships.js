29aed865c9e2c52a0a1cdfb6d3dcc074cdf97114
jsmnglng <jsmn.glng@gmail.com>
2019-04-11 12:33:37 -0700

    Fixed duplicate pk value in friendships seed

--- seeds/04_friendships.js
+++ seeds/04_friendships.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('friendships').del()
-    .then(function () {
+  return knex('friendships')
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex('friendships').insert([
-        {id: 1, user_id: 3, friend_id: 1},
-        {id: 2, user_id: 3, friend_id: 2},
-        {id: 3, user_id: 3, friend_id: 4}
+        { user_id: 3, friend_id: 1 },
+        { user_id: 3, friend_id: 2 },
+        { user_id: 3, friend_id: 4 }
       ]);
     });
 };

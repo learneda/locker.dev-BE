d121f2cadc85f68e5142169db41dbbdd3598b9eb
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-11 11:56:55 -0700

    socket broadcast & emitted after inserting data into comments table

--- components/comments/commentControllers.js
+++ components/comments/commentControllers.js
@@ -1,5 +1,5 @@
 const db = require('../../dbConfig');
-
+const io = require('../../index');
 module.exports = {
   async getAllComments (req, res, next) {
     try {
@@ -8,6 +8,36 @@ module.exports = {
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
+    }
+  },
+  async insertComment (req, res, next) {
+    const user_id = req.user.id
+    if (user_id) {
+      try {
+        io.on('connection', (socket) => {
+          socket.on('comments', (msg) => {
+            console.log('response',msg);
+            // emit them right back to all the users listening to this connection
+            if (msg.action === 'create') {
+              db('comments')
+                .insert({
+                  content:msg.content,
+                  user_id: msg.user_id,
+                  post_id: msg.post_id
+                })
+                .then((res) =>{
+                  socket.broadcast.emit('comments', msg);        
+                });
+            }
+            // io.emit('comments', msg);
+          });
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(417).json({msg: 'dont have access'});
     }
   }
 }

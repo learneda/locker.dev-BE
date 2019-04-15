59eb2d08cb011102d8fbd2e03bc84aee14f7f3d3
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-15 11:40:10 -0700

    created getOauthAccounts handler

--- components/auth/authControllers.js
+++ components/auth/authControllers.js
@@ -19,5 +19,21 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       res.redirect('https://learnedadev.netlify.com');
     } else res.redirect('http://localhost:3000');
+  },
+  async getOauthAccounts(req, res, next) {
+    const user_id = req.user.id;
+    const selectPromise = await db('users').select('github_id', 'google_id');
+    try {
+      if (selectPromise) {
+        res.status(200).json(selectPromise);
+      }
+      else {
+        res.status(200).json({msg: 'something went wrong'});
+      }
+    }
+    catch (err) {
+      console.log(err)
+      res.status(500).json(err);
+    }
   }
 };

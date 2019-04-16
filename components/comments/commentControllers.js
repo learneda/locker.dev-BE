f6e4db5eafaba637ab2f3e4b437cecf85a292f7f
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-16 03:34:05 -0700

    created deleteComment handler func

--- components/comments/commentControllers.js
+++ components/comments/commentControllers.js
@@ -39,5 +39,23 @@ module.exports = {
     } else {
       res.status(417).json({msg: 'dont have access'});
     }
+  },
+  async deleteComment (req, res, next) {
+    const user_id = req.user.id
+    if (user_id) {
+      try {
+        const deletePromise = await db('comments').where('id', req.body.comment_id).del();
+        if (deletePromise) {
+          res.status(200).json({msg: 'comment successfully deleted'});
+        } else {
+          res.status(317).json({msg: 'something went wrong'});
+        }
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(500).json({msg: 'no user_id'});
+    }
   }
 }

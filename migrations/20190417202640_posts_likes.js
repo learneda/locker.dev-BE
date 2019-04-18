a7b0f2ede551ad7418e050a87a67256d291e76c2
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-17 20:33:44 -0700

    created post_likes schema

---
+++ migrations/20190417202640_posts_likes.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('posts_likes', tbl => {
+    tbl.increments('id');
+    tbl.integer('post_id');
+    tbl.integer('user_id');
+    tbl.timestamp('created_at').defaultTo(knex.fn.now());
+    tbl.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex, Promise) {
+  
+};

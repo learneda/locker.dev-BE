4474794ddaef7ff0c77bf9cec61b0d94899e2b7a
luiscmartinez <mrluismartinezzz@gmail.com>
2019-04-17 20:25:06 -0700

    removed column

--- migrations/20190321125809_posts.js
+++ migrations/20190321125809_posts.js
@@ -10,7 +10,6 @@ exports.up = function(knex, Promise) {
       .onUpdate('cascade')
       .unsigned();
     tbl.integer('folder_id');
-    tbl.integer('likes');
     tbl.boolean('liked').defaultTo(false);
     tbl.boolean('completed').defaultTo(false);
     tbl.integer('rating').nullable();

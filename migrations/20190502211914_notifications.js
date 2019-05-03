feb2516700bc248dd60ede8b8682cade93f5a869
luis <mrluismartinezzz@gmail.com>
2019-05-02 21:38:54 -0700

    set up notification table structure

--- migrations/20190502211914_notifications.js
+++ migrations/20190502211914_notifications.js
@@ -1,6 +1,22 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('notifications', tbl => {
+      tbl.increments('id')
+
+      tbl.integer('user_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('cascade');
+
+      tbl.integer('post_id')
+        .references('id')
+        .inTable('posts')
+        .onDelete('cascade');
+
+      tbl.boolean('read').defaultTo(false)
+
+      tbl.string('type')
+  })
 };
 
 exports.down = function(knex, Promise) {

a4cb13f3134b8cade58150c909f52f124f0a79a3
cesarnml <cmejia@gmail.com>
2019-05-11 03:32:11 -0700

    pretty

--- components/goodreads/goodreadControllers.js
+++ components/goodreads/goodreadControllers.js
@@ -1,33 +1,24 @@
 require('dotenv').config();
 const cheerio = require('cheerio');
 const axios = require('axios');
-<<<<<<< HEAD
 const xpath = require('xpath');
 const dom = require('xmldom').DOMParser;
-=======
 const db = require('../../dbConfig');
 var books = require('google-books-search');
-
-
-var xpath = require('xpath'),
-  dom = require('xmldom').DOMParser;
->>>>>>> 2fc4b5034a4673ee3cd865f751f37a15d2906ee0
 
 module.exports = {
   async searchBooks(req, res, next) {
     if (req.body.q) {
       books.search(req.body.q, function(error, results) {
-        if ( ! error ) {
-            console.log(results);
-            res.status(200).json(results)
+        if (!error) {
+          console.log(results);
+          res.status(200).json(results);
         } else {
-            console.log(error);
+          console.log(error);
         }
       });
     } else {
-      res.json({err: 'q required in body of post request'})
+      res.json({ err: 'q required in body of post request' });
     }
-  },
-}
-
-
+  }
+};

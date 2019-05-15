56825911c5c2eb20abcb94234001a5000fb3ff54
luis <mrluismartinezzz@gmail.com>
2019-05-15 11:19:45 -0700

    added item_id column to pocket tbl

--- components/pocket/pocketControllers.js
+++ components/pocket/pocketControllers.js
@@ -1,45 +1,43 @@
-require('dotenv').config();
-const axios = require('axios');
-var qs = require('qs');
+require('dotenv').config()
+const axios = require('axios')
+var qs = require('qs')
 
-const db = require('../../dbConfig');
-console.log(process.env.POCKET_KEY);
+const db = require('../../dbConfig')
+console.log(process.env.POCKET_KEY)
 
-var token;
+var token
 
 module.exports = {
 	async login(req, res, next) {
-		const redirect_uri = 'http://localhost:8000/api/pocket/cb';
+		const redirect_uri = 'http://localhost:8000/api/pocket/cb'
 		axios
 			.post('https://getpocket.com/v3/oauth/request', {
 				consumer_key: process.env.POCKET_KEY,
 				redirect_uri: redirect_uri
 			})
 			.then((result) => {
-				console.log('my response', result.data);
-				const code = result.data;
-				token = code.split('=')[1];
+				console.log('my response', result.data)
+				const code = result.data
+				token = code.split('=')[1]
 
-				console.log(code.split('=')[1]);
-				res.redirect(
-					`https://getpocket.com/auth/authorize?request_token=${token}&redirect_uri=${redirect_uri}`
-				);
-			});
+				console.log(code.split('=')[1])
+				res.redirect(`https://getpocket.com/auth/authorize?request_token=${token}&redirect_uri=${redirect_uri}`)
+			})
 	},
 	async pocketCB(req, res, next) {
-		console.log('\n line 23:', token);
+		console.log('\n line 23:', token)
 		if (req.user) {
-			await db('pocket').del().where('user_id', req.user.id);
+			await db('pocket').del().where('user_id', req.user.id)
 			axios
 				.post('https://getpocket.com/v3/oauth/authorize', {
 					consumer_key: process.env.POCKET_KEY,
 					code: token
 				})
 				.then((response) => {
-					console.log('this is response.data', response.data);
-					const encodedResponse = response.data;
-					console.log(qs.parse(encodedResponse));
-					const decoded = qs.parse(encodedResponse);
+					console.log('this is response.data', response.data)
+					const encodedResponse = response.data
+					console.log(qs.parse(encodedResponse))
+					const decoded = qs.parse(encodedResponse)
 					axios
 						.post('https://getpocket.com/v3/get', {
 							consumer_key: process.env.POCKET_KEY,
@@ -47,7 +45,7 @@ module.exports = {
 						})
 						.then(async (result) => {
 							for (post in result.data.list) {
-								const obj = result.data.list[post];
+								const obj = result.data.list[post]
 								await db('pocket')
 									.insert({
 										resolved_title: obj.resolved_title,
@@ -56,19 +54,20 @@ module.exports = {
 										top_image_url: obj.top_image_url,
 										favorited: +obj.time_favorited === 0 ? false : true,
 										top_image_url: obj.top_image_url,
-										user_id: req.user.id
+										user_id: req.user.id,
+										item_id: +obj.item_id
 									})
 									.returning('*')
 									.then(async (result) => {
 										await db('locker').insert({
 											user_id: req.user.id,
 											pocket_id: result[0].id
-										});
-									});
+										})
+									})
 							}
-							res.redirect('http://localhost:3000/home/locker');
-						});
-				});
+							res.redirect('http://localhost:3000/home/locker')
+						})
+				})
 		}
 	}
-};
+}
--- migrations/20190513194346_pocket.js
+++ migrations/20190513194346_pocket.js
@@ -1,17 +1,18 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('pocket', (tbl) => {
-		tbl.increments('id');
-		tbl.integer('user_id');
-		tbl.text('resolved_title').nullable(); // title
-		tbl.text('resolved_url'); // url
-		tbl.boolean('favorited').defaultTo(false); // if !0 => true
-		tbl.text('top_image_url'); // img
-		tbl.text('excerpt'); // description
-		tbl.timestamp('created_at').defaultTo(knex.fn.now());
-		tbl.timestamp('updated_at').defaultTo(knex.fn.now());
-	});
-};
+		tbl.increments('id')
+		tbl.integer('user_id')
+		tbl.text('resolved_title').nullable() // title
+		tbl.text('resolved_url') // url
+		tbl.boolean('favorited').defaultTo(false) // if !0 => true
+		tbl.text('top_image_url') // img
+		tbl.text('excerpt') // description
+		tbl.integer('item_id')
+		tbl.timestamp('created_at').defaultTo(knex.fn.now())
+		tbl.timestamp('updated_at').defaultTo(knex.fn.now())
+	})
+}
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTableIfExists('pocket');
-};
+	return knex.schema.dropTableIfExists('pocket')
+}

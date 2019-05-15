0cca44afeca687835620a542fed18db3e4954822
luis <mrluismartinezzz@gmail.com>
2019-05-14 19:49:45 -0700

    component Locker skeleton logic

---
+++ components/locker/lockerControllers.js
@@ -0,0 +1,21 @@
+const db = require('../../dbConfig')
+
+module.exports = {
+	async getLocker(req, res, next) {
+		console.log(req.user.id)
+		if (req.user) {
+			const pocketLocker = await db('locker as l')
+				.where('l.user_id', req.user.id)
+				.join('pocket as p', 'p.id', 'l.pocket_id')
+
+			const goodreadsLokcer = await db('locker as l').join('goodreads as g', 'g.id', 'l.goodreads_id')
+
+			const locker = [ ...goodreadsLokcer, ...pocketLocker ]
+
+			if (locker) {
+				console.log(locker)
+				res.status(200).json(locker)
+			}
+		}
+	}
+}
--- components/pocket/pocketRoutes.js
+++ components/pocket/pocketRoutes.js
@@ -5,7 +5,4 @@ router.get('/', controllers.login);
 
 router.get('/cb', controllers.pocketCB);
 
-// router.get('/shelfs', controllers.getUserShelf);
-
-
 module.exports = router;

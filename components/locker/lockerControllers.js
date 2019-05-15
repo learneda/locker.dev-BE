5678829ed6d115bc8de07f0596f6488af89b1f73
luis <mrluismartinezzz@gmail.com>
2019-05-14 23:24:14 -0700

    fix spelling typo

--- components/locker/lockerControllers.js
+++ components/locker/lockerControllers.js
@@ -8,9 +8,9 @@ module.exports = {
 				.where('l.user_id', req.user.id)
 				.join('pocket as p', 'p.id', 'l.pocket_id')
 
-			const goodreadsLokcer = await db('locker as l').join('goodreads as g', 'g.id', 'l.goodreads_id')
+			const goodreadsLocker = await db('locker as l').join('goodreads as g', 'g.id', 'l.goodreads_id')
 
-			const locker = [ ...goodreadsLokcer, ...pocketLocker ]
+			const locker = [ ...goodreadsLocker, ...pocketLocker ]
 
 			if (locker) {
 				console.log(locker)

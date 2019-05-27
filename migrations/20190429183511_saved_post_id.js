a4408d184b7d32fce4867643c0e92b89d3e0853c
luis <mrluismartinezzz@gmail.com>
2019-05-27 03:58:59 -0700

    created new column on saved_post_id tbl

--- migrations/20190429183511_saved_post_id.js
+++ migrations/20190429183511_saved_post_id.js
@@ -1,15 +1,22 @@
 exports.up = function(knex, Promise) {
+  // entry to this tbl will only happen when creating a new record in posts tbl
+  // from an existing user's posts record
   return knex.schema.createTable('saved_post_id', tbl => {
     tbl.increments('id')
     tbl.integer('user_id')
+    // post_id = is the id of the original record
     tbl
       .integer('post_id')
       .references('id')
       .inTable('posts')
       .onDelete('cascade')
-
+    // saved_from_id = is id of the user who owns the original record
     tbl.integer('saved_from_id')
+    // will always be true ....
     tbl.boolean('saved_to_profile').defaultTo(true)
+
+    // the id of the newly created record in posts tbl that originated from post_id record
+    tbl.integer('created_post_id')
 
     tbl.timestamp('created_at').defaultTo(knex.fn.now())
     tbl.timestamp('updated_at').defaultTo(knex.fn.now())

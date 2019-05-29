da9d2c8a26811d03e8edad1b9c38d18a1485451f
luis <mrluismartinezzz@gmail.com>
2019-05-29 10:32:07 -0700

    populated types tbl with seed file

---
+++ seeds/05_types.js
@@ -0,0 +1,15 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('types')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('types').insert([
+        { id: 1, type_title: 'article' },
+        { id: 2, type_title: 'course' },
+        { id: 3, type_title: 'book' },
+        { id: 4, type_title: 'video' },
+        { id: 5, type_title: 'podcast' },
+      ])
+    })
+}

ee9656066a19f303c31527fbd1fbb6e9cc2b251b
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-06-12 22:32:34 -0700

    faker users and friendships curtailed due to row limit on hobby db

--- components/services/servicesRoutes.js
+++ components/services/servicesRoutes.js
@@ -8,6 +8,7 @@ router.get('/articles', routeCache.cacheSeconds(60), controllers.getArticles)
 
 router.get('/articles/all', controllers.getAllArticles)
 
+router.post('/articles/all', controllers.postAllArticles)
 router.get('/cheerio', controllers.launchCheerio)
 
 module.exports = router

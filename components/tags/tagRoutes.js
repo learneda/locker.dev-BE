592b7ad71ff9b50cc2911282c3bad0056864c39f
luis <mrluismartinezzz@gmail.com>
2019-06-14 01:13:59 -0700

    created controller to unfollow tag

--- components/tags/tagRoutes.js
+++ components/tags/tagRoutes.js
@@ -3,5 +3,8 @@ const controllers = require('./tagControllers')
 
 router.get('/:tag', controllers.getTagPost)
 
-router.post('/follow', controllers.createFriendship)
+router.post('/follow', controllers.followTag)
+
+router.delete('/unfollow', controllers.unfollowTag)
+
 module.exports = router

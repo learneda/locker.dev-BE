cb6a8fc59c2556cd0593ae6bde167c8ccc8d4b86
luis <mrluismartinezzz@gmail.com>
2019-06-17 04:36:04 -0700

    created upload header route

--- components/images/imageControllers.js
+++ components/images/imageControllers.js
@@ -37,6 +37,27 @@ module.exports = {
         .catch(err => console.log(err))
     })
   },
+  async uploadHeader(req, res, next) {
+    upload(req, res, function(err) {
+      if (err) {
+        console.log(err)
+      }
+      console.log(req.file)
+      if (req.file.secure_url) {
+        db('users')
+          .update('header_picture', req.file.secure_url)
+          .where('id', req.user.id)
+          .returning('*')
+          .then(response => {
+            console.log('from this one ? wtf', response[0])
+            res.status(200).json({ success: 'added image', user: response[0] })
+          })
+          .catch(err => console.log(err))
+      } else {
+        res.status(200).json({ success: 'added image' })
+      }
+    })
+  },
   async getImg(req, res, next) {
     try {
       const user_id = req.user === undefined ? req.body.user_id : req.user.id

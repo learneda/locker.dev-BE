99844fc6b225d7e517bbb42aee772242b54c1121
Cesar Napoleon Mejia <cesar@prompt.com>
2020-03-21 19:39:42 -0700

    Refactors folderController to use folderHelpers return data

--- components/folders/folderControllers.js
+++ components/folders/folderControllers.js
@@ -1,23 +1,26 @@
 const helpers = require('./folderHelpers')
 
 module.exports = {
-  async createFolder(req, res, next) {
+  async createFolder(req, res) {
     const { folder_name } = req.body
     const user_id = req.user.id
     if (!folder_name) {
       return res
-        .status(404)
+        .status(400)
         .json({ err: 'must include folder_name in the request body.' })
     }
     try {
-      const helper = await helpers.createFolder(user_id, folder_name)
-      return res.status(helper.statusCode).json(helper.response)
-    } catch (err) {
-      return res.status(500).json({ msg: 'fatal error' })
+      const { statusCode, response } = await helpers.createFolder(
+        user_id,
+        folder_name
+      )
+      return res.status(statusCode).json(response)
+    } catch ({ statusCode, response }) {
+      return res.status(statusCode).json(response)
     }
   },
 
-  async addPost(req, res, next) {
+  async addPost(req, res) {
     const { post_id, folder_id } = req.body
     const user_id = req.user.id
     if (!post_id && !folder_id) {
@@ -26,24 +29,28 @@ module.exports = {
       })
     }
     try {
-      const helper = await helpers.addItemToFolder(user_id, req.body)
-      return res.status(helper.statusCode).json(helper.response)
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ msg: 'fatal error' })
+      const { statusCode, response } = await helpers.addItemToFolder(
+        user_id,
+        req.body
+      )
+      return res.status(statusCode).json(response)
+    } catch ({ statusCode, response }) {
+      return res.status(statusCode).json(response)
     }
   },
   // make sure num is a number
-  async getUserFolders(req, res, next) {
+  async getUserFolders(req, res) {
     const user_id = req.params.id
     try {
-      const helper = await helpers.getUserFolders(Number(user_id))
-      return res.status(helper.statusCode).json(helper.response)
-    } catch (err) {
-      return res.status(500).json({ msg: 'fatal error' })
+      const { statusCode, response } = await helpers.getUserFolders(
+        Number(user_id)
+      )
+      return res.status(statusCode).json(response)
+    } catch ({ statusCode, response }) {
+      return res.status(statusCode).json(response)
     }
   },
-  async getPostsByFolderId(req, res, next) {
+  async getPostsByFolderId(req, res) {
     const folder_id = req.params.id
     if (!folder_id) {
       return res
@@ -51,37 +58,37 @@ module.exports = {
         .json({ err: 'must include folder_id in query string..' })
     }
     try {
-      const helper = await helpers.getPostsByFolderId(Number(folder_id))
-      return res.status(helper.statusCode).json(helper.response)
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ msg: 'fatal error' })
+      const { statusCode, response } = await helpers.getPostsByFolderId(
+        Number(folder_id)
+      )
+      return res.status(statusCode).json(response)
+    } catch ({ statusCode, response }) {
+      return res.status(statusCode).json(response)
     }
   },
-  async updateFolderByFolderId(req, res, next) {
+  async updateFolderByFolderId(req, res) {
     const folder_id = req.params.id
     const name = req.body.name
     const user_id = req.user.id
 
-    if (!folder_id && folder_name) {
+    if (!folder_id && name) {
       return res.status(400).json({
         err:
-          'must include folder_id in query string, and folder_name in the body',
+          'must include folder_id in query string, and folder name in the body',
       })
     }
     try {
-      const updatedFolder = await helpers.updateSingleFolder(
+      const { statusCode, response } = await helpers.updateSingleFolder(
         folder_id,
         name,
         user_id
       )
-      return res.status(updatedFolder.statusCode).json(updatedFolder.response)
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ msg: 'fatal error' })
+      return res.status(statusCode).json(response)
+    } catch ({ statusCode, response }) {
+      return res.status(statusCode).json(response)
     }
   },
-  async deleteFolderByFolderId(req, res, next) {
+  async deleteFolderByFolderId(req, res) {
     const folder_id = req.params.id
     const user_id = req.user.id
 
@@ -92,11 +99,13 @@ module.exports = {
     }
 
     try {
-      const helper = await helpers.deleteFolderByFolderId(folder_id, user_id)
-      return res.status(helper.statusCode).json(helper.response)
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ msg: 'fatal error' })
+      const { statusCode, response } = await helpers.deleteFolderByFolderId(
+        folder_id,
+        user_id
+      )
+      return res.status(statusCode).json(response)
+    } catch ({ statusCode, response }) {
+      return res.status(statusCode).json(response)
     }
   },
 }

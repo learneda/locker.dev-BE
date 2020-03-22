3d9406923c065bd1aa1a917c61c6af0ce315621d
Cesar Napoleon Mejia <cesar@prompt.com>
2020-03-21 21:38:36 -0700

    minor

--- config/configureSocket.js
+++ config/configureSocket.js
@@ -1,7 +1,7 @@
 const db = require('../dbConfig')
 
 /**
- * @param socket {SocketIO.Server} socket-io instance
+ * @param io {SocketIO.Server} socket-io instance
  * @description Helper function that configures socket-io instance
  */
 exports.configureSocket = io => {

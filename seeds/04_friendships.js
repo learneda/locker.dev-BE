aafa059adf86f9d1dacb2a888c73c9573ec0020c
Luis Martinez <mrluismartinezzz@gmail.com>
2020-04-01 13:28:42 -0700

    [seeds] creating friendships seeds

--- seeds/04_friendships.js
+++ seeds/04_friendships.js
@@ -1,18 +1,23 @@
-const friends = Array(97)
-  .fill(null)
-  .map((_, index) => index + 1)
-const fakeInsertData = friends.reduce((acc, curr, i) => {
-  const friendshipID = friends
-    .slice(i + 1)
-    .splice(Math.floor(Math.random() * friends.length), 1)
-    .pop()
-  const friendshipObj = {
-    user_id: i + 1,
-    friend_id: friendshipID || i + 2,
-  }
+const fakeInsertData = []
 
-  return [...acc, friendshipObj]
-}, [])
+const n_users = 99
+
+for (let i = 1; i < 100; i += 1) {
+  // array with all fake users id: 1 to 99
+  const friends = [...Array(n_users)].map((_, index) => index + 1)
+  // remove current user as a possible friend
+  friends.splice(i - 1, 1)
+  for (let j = 0; j < 5; j += 1) {
+    // picks a new friend from friends array and
+    // removes pickedId from friends array
+    const friendId = friends.splice(Math.floor(Math.random() * friends.length), 1).pop()
+    const friendShip = {
+      user_id: i,
+      friend_id: friendId,
+    }
+    fakeInsertData.push(friendShip)
+  }
+}
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('friendships')

f6083f284c5573426f9b1fa46743050103973ef4
Luis Martinez <mrluismartinezzz@gmail.com>
2020-04-01 04:12:31 -0700

    [migration] fixed integrationTbl drop function

--- migrations/20190514123128_integrations.js
+++ migrations/20190514123128_integrations.js
@@ -1,30 +1,32 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('integrations', tbl => {
-    tbl.increments('id')
-    tbl
-      .integer('user_id')
-      .references('id')
-      .inTable('users')
-      .onDelete('cascade')
-      .onUpdate('cascade')
-      .notNullable()
+  return knex.schema.dropTableIfExists('integrations').then(() => {
+    return knex.schema.createTable('integrations', tbl => {
+      tbl.increments('id')
+      tbl
+        .integer('user_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('cascade')
+        .onUpdate('cascade')
+        .notNullable()
 
-    tbl
-      .integer('pocket_id')
-      .references('id')
-      .inTable('pocket')
-      .onDelete('cascade')
-      .onUpdate('cascade')
+      tbl
+        .integer('pocket_id')
+        .references('id')
+        .inTable('pocket')
+        .onDelete('cascade')
+        .onUpdate('cascade')
 
-    tbl
-      .integer('goodreads_id')
-      .references('id')
-      .inTable('goodreads')
-      .onDelete('cascade')
-      .onUpdate('cascade')
+      tbl
+        .integer('goodreads_id')
+        .references('id')
+        .inTable('goodreads')
+        .onDelete('cascade')
+        .onUpdate('cascade')
+    })
   })
 }
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('locker')
+  return knex.schema.dropTableIfExists('integrations')
 }

beb9aaa3887673bf9d8534dfda358a7600ac2eda
Luis Martinez <mrluismartinezzz@gmail.com>
2020-03-31 22:48:45 -0700

    logServerPrompt without console.log

--- utils/logServerPrompt.js
+++ utils/logServerPrompt.js
@@ -1,10 +1,11 @@
 const c = require('chalk')
+const process = require('process')
 /**
  * @param port server port number defaults to 8000
  * @description Server awakens
  */
 const logServerPrompt = port => {
-  console.log(
+  process.stdout.write(
     `\n 🦄 ${c.red('===')}${c.white('===')}${c.blue('===')} ${c.yellow('localhost')} ${c.cyan(
       'listens and obeys'
     )} ${c.magenta(`on port: ${port}`)} ${c.red('===')}${c.white('===')}${c.blue('===')}  🚀\n`

91b617a620b9a4985a92f7250dca4f11b3c7f870
luis martinez <mrluismartinezzz@gmail.com>
2021-07-27 16:27:53 -0700

    transformed controller func into an async

--- components/listen/listenControllers.js
+++ components/listen/listenControllers.js
@@ -3,9 +3,8 @@ const axios = require('axios')
 module.exports = {
   async search(req, res, next) {
     const { q, offset } = req.body
-
-    axios
-      .get('https://listen-api.listennotes.com/api/v2/search', {
+    try {
+      const result = await axios.get('https://listen-api.listennotes.com/api/v2/search', {
         headers: {
           'X-ListenAPI-Key': process.env.LISTEN_API_KEY,
         },
@@ -17,11 +16,14 @@ module.exports = {
           language: 'English',
         },
       })
-      .then(result =>
-        res.status(200).json({
-          results: result.data.results,
-          next_offset: result.data.next_offset,
-        })
-      )
+
+      res.status(200).json({
+        results: result.data.results,
+        next_offset: result.data.next_offset,
+      })
+    } catch (err) {
+      const { status, data } = err.response
+      res.status(status).json(data)
+    }
   },
 }
